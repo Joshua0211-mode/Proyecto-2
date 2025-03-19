d4144a3535f1d9c04f426c98f35e23c7c21ef807
Joshua0211 <joshuaherrera0211@gmail.com>
2025-03-18 23:16:23 -0600

    El resto de la tabla clientes

---
+++ api/eliminarCliente.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+require_once 'conexion.php';
+$obj = json_decode(file_get_contents("php://input"));
+$stmt = $db->prepare("DELETE FROM clientes where id=?");
+$stmt->bind_param('s',$obj->id);
+$stmt->execute();
+$stmt->close();
+echo "Registro eliminado";
+?>
